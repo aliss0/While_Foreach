9c6642ba54f9d781d6994b0a86c8283f32d634a3
Ali Uygun <aliuygnnn@gmail.com>
2022-11-20 11:16:37 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // while döngüsü
+            Console.Write("Bir Sayı Giriniz : ");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while(sayac <= sayi )
+            {
+                toplam += sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam / sayi);
+            Console.WriteLine("------------------------------");
+            // a dan z ye kadar tüm harfleri yazdırma 
+            char karakter = 'a';
+            while(karakter < 'z')
+            {
+                Console.Write(karakter);
+                karakter++;
+            }
+            Console.WriteLine("");
+            Console.WriteLine("----------------Foreach--------------");
+            string[] arabalar = { "BMW", "FORD", "TOYOTA", "NİSSAN" };
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+
+            Console.ReadLine();
+
+        }
+       
+        
+    }
+}
